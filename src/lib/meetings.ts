e676fa91a312ca7edf207e259ff0a7df25488e63
OhanaDeveloper <dev@ohanarecovery.org>
2025-11-09 11:27:42 -0800

    Change Zoom link in MEETING_INFO
    Updated the Zoom link for the meeting information to encrypted link.

--- src/lib/meetings.ts
+++ src/lib/meetings.ts
@@ -1,5 +1,5 @@
 export const MEETING_INFO = {
-  zoomLink: "https://us06web.zoom.us/j/85215704370?pwd=waffles", // replace with your real link
+  zoomLink: "https://us06web.zoom.us/j/85215704370?pwd=AfvqYFXUgGmCeC6dCr6bieNAw070bs.1", // replace with your real link
   startHour: 23, // 11 PM Pacific
   endHour: 3,    // 3 AM Pacific
   timeZone: "America/Los_Angeles",
